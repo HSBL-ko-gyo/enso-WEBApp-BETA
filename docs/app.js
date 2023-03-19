a5ab4ce7cccd13209554b4ce3b25798ce6277cbb
HSBL-ko-gyo <128065816+HSBL-ko-gyo@users.noreply.github.com>
2023-03-19 13:57:03 +0900

    全体的にコメント追加,整形

--- docs/app.js
+++ docs/app.js
@@ -1,35 +1,37 @@
-//背景系
+// 背景系
 const canvas = document.getElementById('flame');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 
-const backgroundFlickerStrength = 0.9; // 背景のちらつきの強さ
+// 背景のちらつきの強さを設定
+const backgroundFlickerStrength = 0.9;
 const brightnessVariationFactor = 0.1;
 const backgroundAdaptationFrames = 30;
 const minBrightnessVariation = 0.1; // 0.1 から 0.9 までの変動範囲
 const maxBrightnessVariation = 0.9;
 
+// フレームカウントを初期化
 let frameCount = 0;
 
+// 行数と列数を設定
 const numberOfRows = 10;
 const numberOfColumns = 10;
-
 
 // 現在の背景の明るさを格納する変数
 let currentBackgroundBrightness = 128;
 
+// パーティクルの配列を初期化
 const particles = [];
 
 // フェードイン速度を制御する変数
 const fadeInSpeed = 0.01;
 const framesToReachFullAlpha = 60;
 
-const fadeInFrames = 60;      // フェードインにかかるフレーム数
-const displayFrames = 60;     // パーティクルが表示されるフレーム数
-const fadeOutFrames = 60;     // フェードアウトにかかるフレーム数
-
-
+// フェードイン、表示、フェードアウトにかかるフレーム数を設定
+const fadeInFrames = 60;
+const displayFrames = 60;
+const fadeOutFrames = 60;
 
 // 炎のサイズを調整する変数
 const flameSize = 2;
@@ -74,23 +76,18 @@ setInterval(() => {
   }
 }, 100);
 
-
-
-
+// ランダムな色を生成する関数
 function getRandomColor() {
-  return `rgba(255, ${Math.floor(Math.random() * 128) + 128}, 0, 0.5)`;
+  return `rgba(255, ${Math.floor(Math.random() * 128) + 128}, 0, 0.5)
+`;
 }
 
+// Particle クラス
 class Particle {
-
-  
-  
   constructor(x, y) {
     // 透明度を追加
     this.alpha = 0;
     this.framesToReachFullAlpha = framesToReachFullAlpha;
-
-
     this.x = canvas.width / 2 + Math.random() * 60 * flameSize - 30 * flameSize;
     this.y = canvas.height * 0.8;
     this.size = (Math.random() * 8 + 4) * flameSize;
@@ -100,28 +97,27 @@ class Particle {
     this.shape = Math.floor(Math.random() * 3); // 0 to 2
     this.wind = 0;
     this.speedX = (Math.random() - 0.5) * particleSpreadLimit;
-    
-        // 加速度を追加
+
+    // 加速度を追加
     this.accelX = 0;
     this.accelY = 0;
 
-    
     // フェードアウト
     this.state = 'fadeIn';       // パーティクルの状態を表すプロパティ
     this.fadeInFrames = fadeInFrames;
     this.displayFrames = displayFrames;
     this.fadeOutFrames = fadeOutFrames;
-    
   }
 
+  // パーティクルを描画する関数
   draw() {
     ctx.globalAlpha = this.alpha;
-    
     ctx.fillStyle = this.color;
     ctx.beginPath();
-    
+
     ctx.globalAlpha = 1;
 
+    // パーティクルの形状に応じて描画方法を変更
     switch (this.shape) {
       case 0: // Square
         ctx.fillRect(this.x, this.y, this.size, this.size);
@@ -140,53 +136,49 @@ class Particle {
     }
   }
 
-
+  // パーティクルの状態を更新する関数
   update() {
-    
-// 状態に応じて透明度を変更
-if (this.state === 'fadeIn') {
-  this.alpha += 1 / this.fadeInFrames;
-  if (this.alpha >= 1) {
-    this.alpha = 1;
-    this.state = 'display';
-  }
-} else if (this.state === 'display') {
-  this.displayFrames--;
-  if (this.displayFrames <= 0) {
-    this.state = 'fadeOut';
-  }
-} else if (this.state === 'fadeOut') {
-  this.alpha -= 1 / this.fadeOutFrames;
-  if (this.alpha <= 0) {
-    this.alpha = 0;
-    this.state = 'hidden';
-  }
-}
-
+    // 状態に応じて透明度を変更
+    if (this.state === 'fadeIn') {
+      this.alpha += 1 / this.fadeInFrames;
+      if (this.alpha >= 1) {
+        this.alpha = 1;
+        this.state = 'display';
+      }
+    } else if (this.state === 'display') {
+      this.displayFrames--;
+      if (this.displayFrames <= 0) {
+        this.state = 'fadeOut';
+      }
+    } else if (this.state === 'fadeOut') {
+      this.alpha -= 1 / this.fadeOutFrames;
+      if (this.alpha <= 0) {
+        this.alpha = 0;
+        this.state = 'hidden';
+      }
+    }
     // カーソルやタッチの位置を避ける
     if (cursorPosition.x && cursorPosition.y) {
       const distanceX = this.x - cursorPosition.x;
       const distanceY = this.y - cursorPosition.y;
       const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-
       if (distance < 100) {
         const avoidanceStrength = 2 * (100 - distance) / 100;
         this.x += (distanceX / distance) * avoidanceStrength;
         this.y += (distanceY / distance) * avoidanceStrength;
       }
     }
+
     this.x += this.speedX + this.wind;
     this.y -= this.speedY * particleRiseSpeed;
     this.size -= particleSizeDecreaseSpeed;
     this.speedY *= 0.995;
-    
   }
 
+  // パーティクルを描画する関数 (アルファ値を適用)
   draw() {
-    
     ctx.fillStyle = this.color;
     ctx.globalAlpha = this.alpha; // 透明度を適用
-
     if (this.shape === 0) {
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
@@ -204,12 +196,16 @@ if (this.state === 'fadeIn') {
     }
     ctx.globalAlpha = this.alpha; // 透明度を適用
   }
+
 }
 
+
+// パーティクルを追加する関数
 function addParticle() {
   particles.push(new Particle());
 }
 
+// パーティクルの状態を更新する関数
 function updateParticles() {
   particles.forEach((particle, index) => {
     particle.update();
@@ -219,6 +215,7 @@ function updateParticles() {
   });
 }
 
+// パーティクルを描画する関数
 function drawParticles() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   particles.forEach(particle => {
@@ -226,6 +223,7 @@ function drawParticles() {
   });
 }
 
+// パーティクルを追加し、描画する関数
 function addAndDrawParticles() {
   let count = 0;
   while (count < 8) {
@@ -234,12 +232,12 @@ function addAndDrawParticles() {
   }
 }
 
+// パーティクルの風の方向を変更する関数
 function changeWind(particle) {
   particle.wind += (Math.random() * 2 - 1) * 0.05;
 }
 
-///*背景系
-
+// 背景を描画する関数
 function calculateAverageFlameBrightness() {
   let totalBrightness = 0;
   let count = 0;
